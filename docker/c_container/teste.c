b1a30bb511af136afc2cd58243a210aa1204920b
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2026-04-17 23:12:51 -0400

    first commit

---
+++ docker/c_container/teste.c
@@ -0,0 +1,22 @@
+// -----------------------------------------------------
+// Script     : teste.c
+// Description:
+// Versio     : 0.1
+// Author     : Luca Gorayeb <lucagorayeb@gmail.com>
+// Date       : 16/04/2026
+// Lincence   : GNU/GPL v3.0
+// ----------------------------------------------------- 
+#include <stdio.h> 
+
+void mostrarMensagem(void);
+
+int main() {
+        mostrarMensagem();
+        return 0;
+}
+
+void mostrarMensagem(void){
+        printf("Bem vindo ao container para C\n");
+        printf("É bom ter você aqui!\n");
+}
+
